6e17fb24b615daca4403b3e6361e23b0ba20b2e8
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-03-19 14:25:33 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { sendDataToGA } from './utils/events';
 const min = 2000;
 const max = 3_000_000;
 
-const chips = [2000, 5000, 15000, 25000];
+const chips = [2000, 5000, 15000, 36000];
 
 const addSome = 36_000;
 const sduiLink =
